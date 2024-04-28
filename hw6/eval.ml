fd226017fe839c5311785cc316e47e06f5020038
Ryang Sohn <loop.infinitely@gmail.com>
2024-04-29 04:13:15 +0900

    feat(hw6): finish

--- hw6/eval.ml
+++ hw6/eval.ml
@@ -4,6 +4,12 @@ exception NotImplemented
 exception Stuck
 exception NotConvertible
 
+module IndexMap = Map.Make (struct
+  type t = index
+
+  let compare = compare
+end)
+
 type stoval = Computed of value | Delayed of exp * env
 and stack = Hole_SK | Frame_SK of stack * frame
 
@@ -12,17 +18,49 @@ and state =
   | Return_ST of stoval Heap.heap * stack * value
 
 (* Define your own datatypes *)
-and env = ()
-and value = ()
-and frame = ()
+and env = Heap.loc IndexMap.t
+
+and value =
+  | True
+  | False
+  | Num of int
+  | Eunit
+  | Closure of env * Tml.exp
+  | Pair of value * value
+  | Inl of value
+  | Inr of value
+  | Plus
+  | Minus
+  | Eq
+
+and frame =
+  | Heap_ref of Heap.loc
+  | App_hole of env * Tml.exp
+  | If_hole of env * Tml.exp * Tml.exp
+  | Pair_fst_hole of env * Tml.exp
+  | Pair_snd_hole of value
+  | Fst_hole
+  | Snd_hole
+  | Inl_hole
+  | Inr_hole
+  | Case_hole of env * Tml.exp * Tml.exp
+  | Plus_hole
+  | Minus_hole
+  | Eq_hole
 
 (* Define your own empty environment *)
-let emptyEnv = ()
+let emptyEnv = IndexMap.empty
 
 (* Implement the function value2exp : value -> Tml.exp
  * Warning : If you give wrong implementation of this function,
  *           you wiil receive no credit for the entire third part! *)
-let value2exp _ = raise NotImplemented
+let value2exp v =
+  match v with
+  | True -> Tml.True
+  | False -> Tml.False
+  | Num n -> Tml.Num n
+  | Eunit -> Tml.Eunit
+  | _ -> raise NotConvertible
 
 module NameSet = Set.Make (String)
 
@@ -208,7 +246,148 @@ let rec step1 te =
 
 (* Problem 3. 
  * step2 : state -> state *)
-let step2 _ = raise NotImplemented
+let step2 st =
+  let shiftEnv env =
+    IndexMap.fold
+      (fun idx hloc acc -> IndexMap.add (idx + 1) hloc acc)
+      env emptyEnv
+  in
+  match st with
+  | Anal_ST (heap, eval_stack, te, eval_env) -> (
+      match te with
+      | Ind v -> (
+          let heap_index = IndexMap.find v eval_env in
+          match Heap.deref heap heap_index with
+          | Computed computed_val -> Return_ST (heap, eval_stack, computed_val)
+          | Delayed (delayed_te, delayed_env) ->
+              Anal_ST
+                ( heap,
+                  Frame_SK (eval_stack, Heap_ref heap_index),
+                  delayed_te,
+                  delayed_env ))
+      | Lam _ -> Return_ST (heap, eval_stack, Closure (eval_env, te))
+      | App (abs_te, arg_te) ->
+          Anal_ST
+            ( heap,
+              Frame_SK (eval_stack, App_hole (eval_env, arg_te)),
+              abs_te,
+              eval_env )
+      | Pair (fst_te, snd_te) ->
+          Anal_ST
+            ( heap,
+              Frame_SK (eval_stack, Pair_fst_hole (eval_env, snd_te)),
+              fst_te,
+              eval_env )
+      | Fst inner_te ->
+          Anal_ST (heap, Frame_SK (eval_stack, Fst_hole), inner_te, eval_env)
+      | Snd inner_te ->
+          Anal_ST (heap, Frame_SK (eval_stack, Snd_hole), inner_te, eval_env)
+      | Eunit -> Return_ST (heap, eval_stack, Eunit)
+      | Inl inner_te ->
+          Anal_ST (heap, Frame_SK (eval_stack, Inl_hole), inner_te, eval_env)
+      | Inr inner_te ->
+          Anal_ST (heap, Frame_SK (eval_stack, Inr_hole), inner_te, eval_env)
+      | Case (case_te, inl_te, inr_te) ->
+          Anal_ST
+            ( heap,
+              Frame_SK (eval_stack, Case_hole (eval_env, inl_te, inr_te)),
+              case_te,
+              eval_env )
+      | Fix fix_te ->
+          let new_heap, fix_loc = Heap.allocate heap (Delayed (te, eval_env)) in
+          Anal_ST
+            ( new_heap,
+              eval_stack,
+              fix_te,
+              IndexMap.add 0 fix_loc (shiftEnv eval_env) )
+      | True -> Return_ST (heap, eval_stack, True)
+      | False -> Return_ST (heap, eval_stack, False)
+      | Ifthenelse (cond_te, then_te, else_te) ->
+          Anal_ST
+            ( heap,
+              Frame_SK (eval_stack, If_hole (eval_env, then_te, else_te)),
+              cond_te,
+              eval_env )
+      | Num n -> Return_ST (heap, eval_stack, Num n)
+      | Plus -> Return_ST (heap, eval_stack, Plus)
+      | Minus -> Return_ST (heap, eval_stack, Minus)
+      | Eq -> Return_ST (heap, eval_stack, Eq))
+  | Return_ST (heap, eval_stack, v) -> (
+      match eval_stack with
+      | Hole_SK -> raise Stuck
+      | Frame_SK (below, top) -> (
+          match top with
+          | Heap_ref loc ->
+              let new_heap = Heap.update heap loc (Computed v) in
+              Return_ST (new_heap, below, v)
+          | App_hole (arg_env, arg_te) -> (
+              match v with
+              | Closure (closure_env, Lam abs_body_te) ->
+                  let new_heap, arg_loc =
+                    Heap.allocate heap (Delayed (arg_te, arg_env))
+                  in
+                  Anal_ST
+                    ( new_heap,
+                      below,
+                      abs_body_te,
+                      IndexMap.add 0 arg_loc (shiftEnv closure_env) )
+              | Plus ->
+                  Anal_ST (heap, Frame_SK (below, Plus_hole), arg_te, arg_env)
+              | Minus ->
+                  Anal_ST (heap, Frame_SK (below, Minus_hole), arg_te, arg_env)
+              | Eq -> Anal_ST (heap, Frame_SK (below, Eq_hole), arg_te, arg_env)
+              | _ -> raise Stuck)
+          | If_hole (cond_env, then_te, else_te) -> (
+              match v with
+              | True -> Anal_ST (heap, below, then_te, cond_env)
+              | False -> Anal_ST (heap, below, else_te, cond_env)
+              | _ -> raise Stuck)
+          | Pair_fst_hole (pair_env, snd_te) ->
+              Anal_ST (heap, Frame_SK (below, Pair_snd_hole v), snd_te, pair_env)
+          | Pair_snd_hole fst_v -> Return_ST (heap, below, Pair (fst_v, v))
+          | Fst_hole -> (
+              match v with
+              | Pair (fst_v, _) -> Return_ST (heap, below, fst_v)
+              | _ -> raise Stuck)
+          | Snd_hole -> (
+              match v with
+              | Pair (_, snd_v) -> Return_ST (heap, below, snd_v)
+              | _ -> raise Stuck)
+          | Inl_hole -> Return_ST (heap, below, Inl v)
+          | Inr_hole -> Return_ST (heap, below, Inr v)
+          | Case_hole (case_env, inl_te, inr_te) -> (
+              match v with
+              | Inl v' ->
+                  let new_heap, inl_loc = Heap.allocate heap (Computed v') in
+                  Anal_ST
+                    ( new_heap,
+                      below,
+                      inl_te,
+                      IndexMap.add 0 inl_loc (shiftEnv case_env) )
+              | Inr v' ->
+                  let new_heap, inr_loc = Heap.allocate heap (Computed v') in
+                  Anal_ST
+                    ( new_heap,
+                      below,
+                      inr_te,
+                      IndexMap.add 0 inr_loc (shiftEnv case_env) )
+              | _ -> raise Stuck)
+          | Plus_hole -> (
+              match v with
+              | Pair (Num lhs, Num rhs) ->
+                  Return_ST (heap, below, Num (lhs + rhs))
+              | _ -> raise Stuck)
+          | Minus_hole -> (
+              match v with
+              | Pair (Num lhs, Num rhs) ->
+                  Return_ST
+                    (heap, below, Num (if lhs < rhs then 0 else lhs - rhs))
+              | _ -> raise Stuck)
+          | Eq_hole -> (
+              match v with
+              | Pair (Num lhs, Num rhs) ->
+                  Return_ST (heap, below, if lhs = rhs then True else False)
+              | _ -> raise Stuck)))
 
 (* exp2string : Tml.exp -> string *)
 let rec exp2string exp =
@@ -239,9 +418,71 @@ let rec exp2string exp =
 (* state2string : state -> string 
  * you may modify this function for debugging your code *)
 let state2string st =
+  let rec value2string v =
+    match v with
+    | True -> "true"
+    | False -> "false"
+    | Num n -> "<" ^ string_of_int n ^ ">"
+    | Eunit -> "()"
+    | Closure (e, te) -> "[" ^ env2string e ^ ", " ^ exp2string te ^ "]"
+    | Pair (v1, v2) -> "(" ^ value2string v1 ^ ", " ^ value2string v2 ^ ")"
+    | Inl v -> "inl " ^ value2string v
+    | Inr v -> "inr " ^ value2string v
+    | Plus -> "+"
+    | Minus -> "-"
+    | Eq -> "="
+  and env2string e =
+    IndexMap.fold
+      (fun idx loc acc ->
+        acc ^ ", " ^ string_of_int idx ^ "=>" ^ string_of_int loc)
+      e "., "
+  and heap2string h =
+    let stoval2string s =
+      match s with
+      | Computed v -> "computed(" ^ value2string v ^ ")"
+      | Delayed (te, e) ->
+          "delayed(" ^ exp2string te ^ ", " ^ env2string e ^ ")"
+    in
+    "["
+    ^ List.fold_left
+        (fun acc (loc, s) ->
+          acc ^ ", " ^ string_of_int loc ^ "=>" ^ stoval2string s)
+        "., " h
+    ^ "]"
+  and stack2string st =
+    let frame2string f =
+      match f with
+      | Heap_ref loc -> "[" ^ string_of_int loc ^ "]"
+      | App_hole (e, te) -> "_" ^ env2string e ^ " " ^ exp2string te
+      | If_hole (e, then_te, else_te) ->
+          "if _" ^ env2string e ^ " then " ^ exp2string then_te ^ " else "
+          ^ exp2string else_te
+      | Pair_fst_hole (e, te) ->
+          "(_" ^ env2string e ^ ", " ^ exp2string te ^ ")"
+      | Pair_snd_hole v -> "(" ^ value2string v ^ ", _)"
+      | Fst_hole -> "fst _"
+      | Snd_hole -> "snd _"
+      | Inl_hole -> "inl _"
+      | Inr_hole -> "inr _"
+      | Case_hole (case_env, inl_te, inr_te) ->
+          "case _" ^ env2string case_env ^ " of inl " ^ exp2string inl_te
+          ^ " | inr " ^ exp2string inr_te
+      | Plus_hole -> "plus _"
+      | Minus_hole -> "minus _"
+      | Eq_hole -> "eq _"
+    in
+    match st with
+    | Hole_SK -> "_"
+    | Frame_SK (st', f) -> stack2string st' ^ "; " ^ frame2string f
+  in
   match st with
-  | Anal_ST (_, _, exp, _) -> "Analysis : ???"
-  | Return_ST (_, _, _) -> "Return : ??? "
+  | Anal_ST (heap, eval_stack, exp, eval_env) ->
+      "Analysis : (heap=" ^ heap2string heap ^ ", stack=["
+      ^ stack2string eval_stack ^ "], exp=" ^ exp2string exp ^ ", env=["
+      ^ env2string eval_env ^ "])"
+  | Return_ST (heap, eval_stack, v) ->
+      "Return : (heap=" ^ heap2string heap ^ ", stack=["
+      ^ stack2string eval_stack ^ "], value=" ^ value2string v ^ ")"
 
 (* ------------------------------------------------------------- *)
 let stepOpt1 e = try Some (step1 e) with Stuck -> None
